4ec1df56c31f2d3fecbe28ae993b9803fd7c3cba
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-10-29 14:20:18 -0400

    update: adjusted menu position, highlighing VS right click

--- src/context.js
+++ src/context.js
@@ -1,16 +1,26 @@
 // - Foreground script -
 import createFeaturesMenu from './scripts/features.js';
-import { setStorage } from './scripts/utils.js';
+import { setStorage} from './scripts/utils.js';
 
 // -- Features Menu --
 const features = createFeaturesMenu();
 const body = document.getElementsByTagName('body');
 body[0].appendChild(features);
 
-const showFeatures = (e) => {
+const showFeatures = (e, selection = null) => {
+
+  // menu appears differently for highlight VS right click
+  if (selection) {
+    const {right, top} = selection.getRangeAt(0).getBoundingClientRect()
+    const pageLeft = document.documentElement.scrollLeft
+    const pageTop = document.documentElement.scrollTop
+    features.style.left = `${(right + pageLeft) - 1}px`
+    features.style.top = `${(top + pageTop) - features.offsetHeight - 3}px`
+  } else {
+    features.style.left = `${e.pageX + 1}px`
+    features.style.top = `${e.pageY - features.offsetHeight - 5}px`
+  }
   features.style.visibility = 'visible';
-  features.style.left = `${e.pageX}px`
-  features.style.top = `${e.pageY}px`
 };
 
 const hideFeatures = () => {
@@ -20,8 +30,14 @@ const hideFeatures = () => {
 // -- Validate Selection --
 // validate and save selection
 const validateSelection = () => {
-  // TODO: what if selection isnt text, what if selection is inside input element
+
   let selection = null;
+
+  console.log('active element:', document.activeElement.tagName)
+  const activeElement = document.activeElement.tagName
+  if (activeElement === 'INPUT' || activeElement === 'TEXTAREA') {
+    return;
+  }
   if (window.getSelection) {
     selection = window.getSelection();
   } else if (document.getSelection) {
@@ -45,9 +61,14 @@ const validateSelection = () => {
 // -- Event Listeners --
 window.addEventListener('click', (event) => {
   // shows or hides features menu
-  if (validateSelection()) {
-    showFeatures(event);
+  const selection = validateSelection()
+  if (selection) {
+    showFeatures(event, selection);
   } else {
     hideFeatures();
   }
 });
+
+window.addEventListener('contextmenu', (event) => {
+  showFeatures(event);
+})
