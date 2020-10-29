b6be4ae4ea771e14bb79ff96e47fb7ae07e6e05a
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-10-29 12:45:18 -0400

    feat: shows and hides menu

--- src/context.js
+++ src/context.js
@@ -4,7 +4,6 @@ import { setStorage } from './scripts/utils.js';
 
 // -- Features Menu --
 const features = createFeaturesMenu();
-console.log('features', features)
 const body = document.getElementsByTagName('body');
 body[0].appendChild(features);
 
@@ -14,7 +13,12 @@ const showFeatures = (e) => {
   features.style.top = `${e.pageY}px`
 };
 
+const hideFeatures = () => {
+  features.style.visibility = 'hidden';
+}
+
 // -- Validate Selection --
+// validate and save selection
 const validateSelection = () => {
   // TODO: what if selection isnt text, what if selection is inside input element
   let selection = null;
@@ -25,6 +29,10 @@ const validateSelection = () => {
   } else if (document.selection) {
     selection = document.selection.createRange();
   }
+  if (selection.type !== 'Range') {
+    selection = null;
+  }
+
   if (selection) {
     const selectionText = selection.text ? selection.text : selection.toString();
     setStorage('selectionText', selectionText);
@@ -36,8 +44,10 @@ const validateSelection = () => {
 
 // -- Event Listeners --
 window.addEventListener('click', (event) => {
-  // validate and save selection
+  // shows or hides features menu
   if (validateSelection()) {
     showFeatures(event);
+  } else {
+    hideFeatures();
   }
 });
